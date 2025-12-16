afd7a9530849292354b017f46ea12735ddada49f
1Hacker-anonimo <160126765+1Hacker-anonimo@users.noreply.github.com>
2025-12-16 17:50:02 -0300

    Commit inicial do projeto

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,8 +1,8 @@
 
 // Configuração do Supabase
 // Substitua URL_DO_SEU_PROJETO e CHAVE_ANONIMA pelos dados do seu projeto no Supabase
-const SUPABASE_URL = 'INSERT_YOUR_SUPABASE_URL_HERE';
-const SUPABASE_KEY = 'INSERT_YOUR_SUPABASE_ANON_KEY_HERE';
+const SUPABASE_URL = 'https://vfgsvfjzmqomkoiuivqg.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmZ3N2Zmp6bXFvbWtvaXVpdnFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5MTQ4NzIsImV4cCI6MjA4MTQ5MDg3Mn0.4UKa9Fzn-_3ofOZ1rrXJRNy0gJN8NuL_P0EwYHB2r70';
 
 // Inicializa o cliente Supabase se a biblioteca estiver carregada
 let supabase;
